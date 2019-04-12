81b5430f4c73b36077e88fa28b05de3aa1ed8b3c
Camilo Calvo <camilocalvoalcaniz@gmail.com>
2019-04-12 08:43:37 -0400

    Real Commit 4

--- index.js
+++ index.js
@@ -104,6 +104,6 @@ app.post("/api/create", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening!');
 });
